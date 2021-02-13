cc1c8c8535fe6c0e47025546e0b663cc6b2af536
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-13 18:00:41 -0300

    :up: Saiu!

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,18 +1,36 @@
+// Define global vars
 const texts = ['Olá, meu nome é Arthur Cabral...', 'Sou desenvolvedor iniciante...', 'Sou Designer...', 'Sou estudante...'];
+var num = 0;
+var li = 0;
+const h1 = document.querySelector('.typing');
+
+// Main Function
 function Type(count){
     let phrase = texts[count];
-    let letters = phrase.split('');
-    let h1 = document.querySelector('.typing');
-    letters.forEach((letras, i) => {
+    let letters = phrase.split(''); // Split the phrase to an array
+    letters.forEach((letras, i) => { // forEach loop to insert the letters into H1
+        li = i  // define li
         setTimeout(function() {
             h1.innerHTML += letras;
-            console.log(letters.length)
-        }, 100 * i);
+        }, 75 * i);
     });
-
-
-    
+    setTimeout(function() {
+        if(li = letters.length) { 
+            if(num === texts.length - 1) { // Validate the num parameter for infinite loop animation
+                num = 0
+            } else {
+                num++ // Increase num value to control what phrase will be returned to Type(val)
+            }
+            h1.innerHTML = ''; // Clean h1
+            Change(num); // Call func
+        }
+    }, 5000);
 };
 
-Type(0)
+// Second func call
+function Change(val) {
+    Type(val)
+}
 
+// First func call, start Main func
+Type(num)
