2d2edd2d17d7a3de26ecbec03e79615eba592b9e
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-16 21:56:04 -0300

    :up: Updating Comments

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,44 +1,85 @@
-// Definindo variáveis globais
+/*
+
+Definindo Variáveis Globais:
+
+*/
+
+//A array texts será inserida na função type, terá por objetivo separar as frases presentes nela.
 const texts = ['Olá, meu nome é Arthur Cabral...', 'Sou desenvolvedor iniciante...', 'Sou Designer...', 'Sou estudante...'];
+
+//A variável num será o índice das frases presente na função
 var num = 0;
+
+//A variável li será o índice das letras de cada frase presente no forEach
 var li = 0;
+
+//A constante h1 será o H1 presente no documento HTML
 const h1 = document.querySelector('.typing');
 
-//Função Type Principal
+/*
+
+    Função Type Principal:
+    Essa função irá separar cada frase da array e cada letra de cada
+    frase, após isso, irá escrever letra por letra dentro do elemento HTML.
+
+*/
+
 function Type(count){
+
+    // Utilizando count para separar cada frase da array
     let phrase = texts[count];
 
-    let letters = phrase.split(''); // Utilizando split para separar cada caractere da frase
+    // Utilizando split para separar cada caractere da frase
+    let letters = phrase.split(''); 
 
-    letters.forEach((letras, i) => { // Utilizando forEach loop para inserir as letras no H1
+    // Utilizando forEach loop para inserir as letras no H1
+    letters.forEach((letras, i) => { 
 
-        li = i  // Definindo li (index)
+        // Definindo li (index)
+        li = i  
 
+        // Utilizando setTimeout para determinar tempo de cada ação * i (quantidade de letras)
         setTimeout(function() {
             h1.innerHTML += letras;
         }, 75 * i);
 
     });
 
-    setTimeout(function() {
+    /*
+
+        A Função Verificar irá utilizar if's para fazer a verificação de
+
+    */
+
+    setTimeout(function verificarLength() {
 
         if(li = letters.length) { 
 
-            if(num === texts.length - 1) { // Validando o parâmetro num para animação de loop infinito   
+            // Validando o parâmetro num para animação de loop infinito
+            if(num === texts.length - 1) {    
                 num = 0
 
             } else {
-                num++ // Aumentando o valor num para controlar qual frase será retornada ao tipo (val)
+                // Aumentando o valor num para controlar qual frase será retornada ao tipo (val)
+                num++ 
             }
 
-            h1.innerHTML = ''; // Limpando H1
-            
-            Change(num); // Chamando a função
+            // Limpando H1
+            h1.innerHTML = ''; 
+
+            // Chamando a função change()
+            Change(num); 
         }
     }, 5000);
 };
 
-// Segunda chamada de função
+/*
+
+    A função change() irá chamar a função Type dentro dela mesma, retornando ao início
+    e fazendo com que o efeito de type seja infinito.
+
+*/
+
 function Change(val) {
     Type(val);
 };
